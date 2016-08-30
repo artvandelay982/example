a4e82b03a6aa178a0cfce6859365151c1f673088
Dylan Williams <ddemora10@gmail.com>
2016-08-29 23:35:03 -0700

    some formatting

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ let bodyParser = require('body-parser');
 let router = module.exports = (server) => new Promise ( (resolve, reject) => {
   bootstrap(server);
   routes(server);
+  
   server.app.listen(server.config.port, (err) => {
     if (err) return reject(err);
     return resolve(server.config.msg.up);
@@ -20,12 +21,14 @@ function routes (server) {
   let lib = server.lib;
   
   app.get('/', (req, res) => res.send(config.msg.hi));
+  
   app.get('/mongo', (req, res) => {
     lib.mongo.listCollections().toArray( (err, docs) => {
       if (err) return res.status(500).send(err);
       return res.status(200).send(docs);
     });
   });
+  
   app.get('/redis', (req, res) => {
     lib.redis.keys( '*', (err, keys) => {
       if (err) return res.status(500).send(err);
